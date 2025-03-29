9279686de103a0d0d0b4b3d934f5444c7bca4f6b
stefanos-zaimis <rd22097@bristol.ac.uk>
2025-03-29 21:08:52 +0000

    .env setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,13 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 
 	"github.com/stef-zaimis/taxa/internal/db"
 	"github.com/stef-zaimis/taxa/internal/api"
 )
 
 func main() {
-	pool := db.Connect()
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found (fallback to system env)")
+	}
+
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL not set")
+	}
+
+	pool := db.Connect(dbURL)
 	defer pool.Close()
 
 	r := api.SetupRouter(pool)
