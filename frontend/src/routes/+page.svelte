b60843f54d16ed9bcb0143df2b1e77fdecedfe32
stefanos-zaimis <rd22097@bristol.ac.uk>
2025-04-02 22:54:24 +0100

    Initial main menu style

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,9 +4,31 @@
 	function startQuiz() {
 		goto('/quiz');
 	}
+
+	function goToOptions() {
+		//TODO
+	}
+
+	function goToSettings() {
+		//TODO
+	}
+
+	function exitGame() {
+		//TODO
+	}
 </script>
 
-<h1>Taxonomic Quiz</h1>
-<p>Welcome. Click on the button below to begin:</p>
+<div class="relative w-screen h-screen overflow-hidden bg-fixed bg-center bg-cover" style="background-image: url('/mm/bg.png');">
 
-<button on:click={startQuiz} style="padding: 1rem 2rem; font-size: 1.25rem; margi-top: 1rem;"> Start Quiz </button>
+	<div class="absolute inset-0 flex flex-col items-center justify-center gap-8 px-4">
+		<img src="/mm/title.png" alt="TAXA Title" class="w-[90vw] max-w-4xl" />
+
+		<img src="/mm/start_button.png" alt="Start Button" class="w-40 md:w-60 cursor-pointer hover:scale-105 transition-transform" on:click{startQuiz} />
+
+		<div class="flex gap-6 pt-4 flex-wrap: wrap">
+			<img src="/mm/options_button.png" alt="Options" class="w-28 md:w-36 cursor-pointer hover:scale-105 transition-transform" on:click={goToOptions} />
+			<img src="/mm/settings_button.png" alt="Settings" class="w-28 md:w-36 cursor-pointer hover:scale-105 transition-transform" on:click={goToSettings} />
+			<img src="/mm/exit_game_button.png" alt="Exit" class="w-28 md:w-36 cursor-pointer hover:scale-105 transition-transform" on:click={exitGame} />
+		</div>
+	</div>
+</div>
